63210c0b41211043a38c4bca4f9ce832b2b8309f
Manojkumargmpa <110841909+Manojkumargmpa@users.noreply.github.com>
2024-03-06 01:23:29 +0530

    csv to backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -22,12 +24,15 @@ def upload_file():
             # Read the CSV file into a DataFrame
             df = pd.read_csv(file_path)
 
+            # Check if the DataFrame has at least one column
+            if df.empty or len(df.columns) == 0:
+                return jsonify({'error': 'No columns to parse'})
+
             # Process the DataFrame as needed
             # For example, you can perform data cleaning, analysis, etc.
 
             # Optionally, you can delete the temporary file
-            # import os
-            # os.remove(file_path)
+            os.remove(file_path)
 
             return jsonify({'message': 'File uploaded successfully', 'dataframe': df.to_dict()})
         except Exception as e:
