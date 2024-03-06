9e017aab60417b83e0b98596ce46a41e1e5c9b8e
Tejeswar Reddy <tejeswarreddysunkugari03@gmail.com>
2024-03-06 22:08:36 +0530

    ok

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
 import os
-
+from datasmells_algorithms.dummy_value import *
 app = Flask(__name__)
 CORS(app)
 
