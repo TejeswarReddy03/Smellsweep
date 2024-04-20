3cde4bd2d4b61e06eaa0a538b3c4a9fe201b5852
Manojkumargmpa <110841909+Manojkumargmpa@users.noreply.github.com>
2024-04-20 22:39:01 +0530

    added refactor method and be able to download csv

---
+++ backend/refactoring_algorithms/Set2_Algos/refactor_int_as_float.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+def sub_refactor_floats_to_int(df, column):
+    # Convert the column to float to ensure that NaNs can be handled
+    df.loc[:, column] = df.loc[:, column].astype(float)
+    
+    # Round the float values to the nearest integer
+    df.loc[:, column] = df.loc[:, column].apply(lambda x: np.round(x) if pd.notnull(x) else x)
+    
+    # Fill NaN values with a default integer (e.g., 0)
+    df.loc[:, column] = df.loc[:, column].fillna(0)
+    
+    # Convert the column to integer
+    convert_dict = {column: int}
+    df = df.astype(convert_dict)
+    
+    return df
+
+def refactor_floats_to_int(df):
+    #print("hi")
+    #print(df.dtypes)
+    for column in df.columns:
+        if any(isinstance(value, (int, float)) for value in df[column]):
+            count_float_represented_as_int = 0
+            count_total_float = 0
+            for value in df[column]:
+                if isinstance(value, float):
+                    count_total_float += 1
+                    if value.is_integer():
+                        count_float_represented_as_int += 1
+
+            if count_total_float > 0:
+                percentage = (count_float_represented_as_int / count_total_float) * 100
+                if percentage > 10:  # If more than 90% values can be represented as integers
+                    df = sub_refactor_floats_to_int(df, column)
+
+    return df
---
+++ backend/refactoring_algorithms/Set2_Algos/refactor_int_as_string.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def refactor_integer_as_string(df):
+    integer_as_string = {}  # Dictionary to store detected instances
+    
+    # Iterate through each column in the DataFrame
+    for column in df.columns:
+        # Check if the column contains any text strings that can be converted to integers
+        integer_count = sum(value.isdigit() for value in df[column] if isinstance(value, str))
+        total_rows = len(df[column])
+        
+        # Calculate the percentage of rows in the column that contain integers stored as strings
+        if integer_count > 0:
+            percentage = (integer_count / total_rows) * 100
+           
+            # Check if the percentage is more than 10%
+            if percentage > 10:
+                # Replace non-integer strings with NaN
+                df[column] = pd.to_numeric(df[column], errors='coerce')
+
+    # Include status attribute
+     # True if any integer-as-string smells were detected, False otherwise
+    return df
+
